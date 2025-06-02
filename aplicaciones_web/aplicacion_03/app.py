44c8da39c6213c837e9cf5f45daef965941f72bc
CHRISTIAN EDUARDO NAVARRO CURIEL <1724110319@utectulancingo.edu.mx>
2025-06-02 13:54:25 +0000

    CREATED configuracion

--- aplicaciones_web/aplicacion_03/app.py
+++ aplicaciones_web/aplicacion_03/app.py
@@ -8,8 +8,12 @@ render = web.template.render('templates')
 app = web.application(urls, globals())
 
 class Index:
+
+    def __init__(self):
+        self.mensaje = "Este es un texto cualquiera"
+
     def GET(self):
-        return render.index()
+        return render.index(self.mensaje)
 
 if __name__ == "__main__":
     app.run()
